7d20697a630521d2db57ce7cd22311c063d3ff04
Lekdo74 <alexandre.giniaux@etu.univ-smb.fr>
2024-01-24 14:40:05 +0100

    -Ajout du model Devise

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,39 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string nomDevise;
+
+		public string NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+        public Devise(int _id, string _nomDevise, double _taux)
+        {
+            Id = _id;
+			NomDevise = _nomDevise;
+			Taux = _taux;
+        }
+        public Devise():this(0, "", 1) { }
+
+
+    }
+}
